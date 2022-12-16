546606a9b683fbcdb5f191c33e1f924a7f0787bf
Suldemka1 <suldemka1@gmail.com>
2022-12-17 06:09:28 +0700

    another changes

--- components/Map/TileLayers.js
+++ components/Map/TileLayers.js
@@ -18,16 +18,13 @@ const TileLayers = () => {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                 </LayersControl.BaseLayer>
-                <LayersControl.Overlay name="Осадки">
-                    <TileLayer
-                        url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=76735a90e4bb65c353382e372da76262`}
-                    />
-                </LayersControl.Overlay>
-                <LayersControl.Overlay name="Погода">
-                    <TileLayer
-                        url={`http://maps.openweathermap.org/maps/2.0/weather/PA0/{z}/{x}/{y}?date=1527811200&appid=76735a90e4bb65c353382e372da76262`}
-                    />
-                </LayersControl.Overlay>
+                
+                <TileLayer
+                    url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=76735a90e4bb65c353382e372da76262`}
+                />
+                <TileLayer
+                    url={`http://maps.openweathermap.org/maps/2.0/weather/PA0/{z}/{x}/{y}?date=1527811200&appid=76735a90e4bb65c353382e372da76262`}
+                />
             </LayersControl>
         </div>
     );
